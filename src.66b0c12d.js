0afbf06d1f3f560b21bcef3addc4e59b2525849f
piotrekjot <81829145+piotrekjot@users.noreply.github.com>
2021-06-16 18:12:45 +0000

    Deploying to gh-pages from @ piotrekjot/team-hellen@fd05849cf803109abc7856f0cc427659474af1cc 🚀

--- src.66b0c12d.js
+++ src.66b0c12d.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-hellen/src.6ca200d5.js.map
+//# sourceMappingURL=/team-hellen/src.66b0c12d.js.map
